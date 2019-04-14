f563db8e262f6f68ec14f92a492611de3327aa5b
A_A <21040751+Otto-AA@users.noreply.github.com>
2019-04-14 18:52:54 +0200

    Updates

---
+++ precache-manifest.9a3e948daedb745d5a96a726af180a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "00c798be29bb91f3d1a6",
+    "url": "./static/js/main.acdae162.chunk.js"
+  },
+  {
+    "revision": "49f331b13d448d9bc51b",
+    "url": "./static/js/2.c31720ba.chunk.js"
+  },
+  {
+    "revision": "00c798be29bb91f3d1a6",
+    "url": "./static/css/main.3639107d.chunk.css"
+  },
+  {
+    "revision": "49f331b13d448d9bc51b",
+    "url": "./static/css/2.fd944c2a.chunk.css"
+  },
+  {
+    "revision": "0472f096d5c94bb60892ca7aefba15f5",
+    "url": "./index.html"
+  }
+];
